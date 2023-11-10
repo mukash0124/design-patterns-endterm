3bfab4f6156d7091977aa36d13f67a2c953f32bc
mukash0124 <mukash.m0124@gmail.com>
2023-11-10 14:53:25 +0600

    Seventh commit

--- memento.go
+++ memento.go
@@ -1,6 +1,19 @@
 package main
 
+type IMemento interface {
+	getTitle() string
+	getDescription() string
+}
+
 type Memento struct {
 	title string
 	description string
+}
+
+func (m *Memento) getTitle () string {
+	return m.title
+}
+
+func (m *Memento) getDescription () string {
+	return m.description
 }
